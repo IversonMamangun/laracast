c5a661597448345a47918bc8a583701a32b9d630
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2025-11-11 12:12:03 +0800

    updating with patch requests

---
+++ controllers/notes/update.php
@@ -0,0 +1,44 @@
+<?php
+use Core\Validator;
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+
+$currentUserId = 4;
+// find the corresponding note
+    $note = $db->query('select * from notes where id = :id', params: [
+        'id' => $_POST['id']
+    ])->findOrFail();
+
+// authorize that the current user can edit the note    
+    authorize($note['users_id'] === $currentUserId);
+
+// Validate the form
+
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+// if no validation errors, update the record in the notes database table.
+if (count($errors)) {
+    return view('laracast/notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+
+    ]);
+}
+
+$db->query('update notes set body = :body where id = :id',[
+    'id' => $_POST['id'],
+    'body' => $_POST['body']
+
+]);
+
+// redirect the user
+header('location: /laracast/notes');
+die();
--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -6,6 +6,7 @@
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <form method="POST" action="/laracast/note" >
             <input type="hidden" name="_method" value="PATCH">
+            <input type="hidden" name="id" value="<?= $note['id']?>">
           <div class="space-y-12">
 
               <div class="col-span-full">
